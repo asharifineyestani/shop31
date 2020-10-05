cb6cd12dc2ced130e4d36bbd709e78cbebac1bf5
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-10-05 21:01:29 +0330

    Added getPossibleEnumValues

---
+++ app/Models/Enum.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Models;
+
+use DB;
+
+
+trait Enum
+{
+    public static function getPossibleEnumValues($name)
+    {
+        $instance = new static; // create an instance of the model to be able to get the table name
+        $type = DB::select(DB::raw('SHOW COLUMNS FROM ' . $instance->getTable() . ' WHERE Field = "' . $name . '"'))[0]->Type;
+        preg_match('/^enum\((.*)\)$/', $type, $matches);
+        $enum = array();
+        foreach (explode(',', $matches[1]) as $value) {
+            $v = trim($value, "'");
+            $enum[] = $v;
+        }
+        return $enum;
+    }
+
+}
